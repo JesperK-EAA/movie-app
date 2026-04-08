d9f99d011b550349a93d40bbba09c72e8aec9ecc
Jesper Kjærgaard <eaa26jekj@students.eaaa.dk>
2026-04-08 14:18:52 +0200

    feat. fetch movie data form json file

--- js/app.js
+++ js/app.js
@@ -1,39 +1,32 @@
 "use strict";
 
-const movies = [
-  {
-    title: "Inception",
-    year: 2010,
-    rating: 8.8,
-  },
-  {
-    title: "The Matrix",
-    year: 1999,
-    rating: 8.7,
-  },
-  {
-    title: "Interstellar",
-    year: 2014,
-    rating: 8.6,
-  },
-  {
-    title: "The Dark Knight",
-    year: 2008,
-    rating: 9.0,
-  },
-];
-
 const movieList = document.getElementById("movie-list");
+let movies = [];
 
-function ShowMovies() {
+getMovieData();
+
+/* Fething the movie data */
+async function getMovieData() {
+  console.log("Henter film data...");
+
+  const res = await fetch("./data/movies.json");
+  movies = await res.json();
+
+  await showMovies();
+}
+
+function showMovies() {
   movies.forEach((movie) => {
     showMovie(movie);
   });
 }
 
 function showMovie(movie) {
+  const highlightClass = movie.rating >= 8.5 ? "movie-card--highlight" : "";
+
   const movieEle = `
-    <article class="movie-card">
+    <article class="movie-card ${highlightClass}">
+      <img class="movie-image" src="${movie.image}" alt="${movie.title}">
       <div class="movie-info">
         <h3>${movie.title}</h3>
         <p>År: ${movie.year}</p>
@@ -45,4 +38,4 @@ function showMovie(movie) {
   movieList.insertAdjacentHTML("beforeend", movieEle);
 }
 
-ShowMovies()
+ShowMovies();
