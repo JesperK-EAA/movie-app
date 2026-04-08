20c48c59bc3e787507713b24111536ea55654635
Jesper Kjærgaard <eaa26jekj@students.eaaa.dk>
2026-04-08 11:06:10 +0200

    feat: movie data and for loop

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
 "use strict";
 
+const movies = [
+  {
+    title: "Inception",
+    year: 2010,
+    rating: 8.8,
+  },
+  {
+    title: "The Matrix",
+    year: 1999,
+    rating: 8.7,
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    rating: 8.7,
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    rating: 9.1,
+  },
+];
+
+movies.forEach(movie => {
+    console.log(movie.title);
+    
+});
