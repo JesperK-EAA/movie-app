38a73f1bbeb19e3892a9eb5ae910b6effe6fc1d6
Jesper Kjærgaard <eaa26jekj@students.eaaa.dk>
2026-04-22 07:44:44 +0200

    feat: day 3 complet

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 const movieList = document.getElementById("movie-list");
 const genreSelecter = document.getElementById("genre-select");
+const movieCount = document.querySelector("#movie-count");
+
 const movieAPI =
   "https://raw.githubusercontent.com/cederdorff/race/refs/heads/master/data/movies.json";
 let allMovies = [];
@@ -22,10 +24,12 @@ async function getMovieData() {
 /* Create options of the movies genre and insert them */
 function populateGenreSelect() {
   const genres = new Set();
+  let genreCount = {};
 
   for (const movie of allMovies) {
     for (const genre of movie.genre) {
       genres.add(genre);
+      genreCount[genre] = (genreCount[genre] || 0) + 1;
     }
   }
 
@@ -34,7 +38,7 @@ function populateGenreSelect() {
   for (const genre of sortedGenres) {
     genreSelecter.insertAdjacentHTML(
       "beforeend",
-      `<option value="${genre}">${genre}</option>`,
+      `<option value="${genre}">${genre} (${genreCount[genre]})</option>`,
     );
   }
 }
@@ -42,7 +46,6 @@ function populateGenreSelect() {
 /* Finds movie data out from the genre */
 function applyGenreFilter() {
   const selectedGenre = genreSelecter.value;
-  console.log("change:", selectedGenre);
 
   if (selectedGenre === "all") {
     showMovies(allMovies);
@@ -63,6 +66,8 @@ function showMovies(movies) {
   for (const movie of movies) {
     showMovie(movie);
   }
+
+  movieCount.textContent = `Viser ${movies.length} film`;
 }
 
 /* Makes the movie cards */
